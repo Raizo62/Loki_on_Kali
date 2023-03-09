7681d6d90e8b62ceb0bf05c8581b01d063e20130
Raizo62 <silicium62-github@yahoo.fr>
2023-03-09 19:33:38 +0100

    Add the scripts "run_loki_bash.sh", "run_loki_gtk.sh" and "run_loki_urw.sh" to run the container "loki_on_kali:latest" with the good rights

---
+++ Docker/run_loki_bash.sh
@@ -0,0 +1,7 @@
+docker run \
+	--privileged --network=host					\
+	--rm										\
+	--volume /tmp/:/tmp/						\
+	--env DISPLAY --env XAUTHORITY='/xauthority' --volume /tmp/.X11-unix/:/tmp/.X11-unix/ --volume ${XAUTHORITY}:/xauthority:ro \
+	--interactive --tty --entrypoint /bin/bash	\
+	loki_on_kali:latest
